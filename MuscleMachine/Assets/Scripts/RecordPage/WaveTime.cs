7e5a700a264d413c1b39bf664be8936b7818ea60
ling <1759147969@qq.com>
2020-08-07 16:53:58 +0800

    添加鼠标滚轮控制

---
+++ MuscleMachine/Assets/Scripts/RecordPage/WaveTime.cs
@@ -0,0 +1,30 @@
+﻿/****************************************************
+    文件：SaveWav.cs
+	作者：Ling
+    邮箱: 1759147969@qq.com
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveTime : MonoBehaviour
+{
+    private readonly string R_SliderText = "Text_Slider";
+    private Slider IntervalSlider;
+
+    private void Start()
+    {
+        IntervalSlider = GetComponentInChildren<Slider>();    
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            IntervalSlider.value += Input.GetAxis("Mouse ScrollWheel") * 20;
+            transform.Find(R_SliderText).GetComponent<Text>().text = (int)IntervalSlider.value + "ms";
+        }
+    }
+}
